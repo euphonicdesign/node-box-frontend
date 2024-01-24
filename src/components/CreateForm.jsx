72d0f2b71aaa000376b5fbefcb5f64864dcfc537
Sorin <zece.delfini@outlook.com>
2024-01-24 20:58:39 +0200

    added table of expenses

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -2,59 +2,73 @@ import { useState } from "react";
 import axios from "axios";
 import "./CreateForm.css";
 
+const resetFormDetails = () => {
+  return {
+    date: "2024-01-01",
+    description: "",
+    value: "",
+    category: "",
+  };
+};
+
 function CreateForm({ handleRequests }) {
-  const [date, setDate] = useState("2024-01-01");
-  const [description, setDescription] = useState("");
-  const [value, setValue] = useState("");
-  const [category, setCategory] = useState("");
+  const [formDetails, setFormDetails] = useState(resetFormDetails());
+
+  // const [date, setDate] = useState("2024-01-01");
+  // const [description, setDescription] = useState("");
+  // const [value, setValue] = useState("");
+  // const [category, setCategory] = useState("");
 
   function handleInput(e) {
-    if (e.target.id === "date") setDate(e.target.value);
-    if (e.target.id === "description") setDescription(e.target.value);
-    if (e.target.id === "value") setValue(e.target.value);
-    if (e.target.id === "category") setCategory(e.target.value);
+    setFormDetails({
+      ...formDetails,
+      [e.target.id]: e.target.value,
+    });
+    // if (e.target.id === "date") setDate(e.target.value);
+    // if (e.target.id === "description") setDescription(e.target.value);
+    // if (e.target.id === "value") setValue(e.target.value);
+    // if (e.target.id === "category") setCategory(e.target.value);
   }
 
   async function handleSubmit(e) {
     e.preventDefault();
     const response = await axios.post(
       "https://node-box.onrender.com/api/v1/expenses",
-      {
-        date,
-        description,
-        value,
-        category,
-      }
+      formDetails
     );
     // console.log(response);
-    setDescription("");
-    setValue("");
-    setCategory("");
+    setFormDetails(resetFormDetails());
+
     handleRequests((prev) => prev + 1);
   }
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="date" value={date} onChange={handleInput} id="date" />
+        <input
+          type="date"
+          value={formDetails.date}
+          onChange={handleInput}
+          id="date"
+        />
         <input
           id="description"
           type="text"
           placeholder="description"
-          value={description}
+          value={formDetails.description}
           onChange={handleInput}
         />
         <input
           id="value"
           type="text"
           placeholder="value"
-          value={value}
+          value={formDetails.value}
           onChange={handleInput}
         />
         <select
           name="category"
           id="category"
-          value={category}
+          value={formDetails.category}
           onChange={handleInput}
         >
           <option value="">Select a category</option>
