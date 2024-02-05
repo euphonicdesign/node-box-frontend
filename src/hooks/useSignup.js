f98c0863c4405b14235b012d68200e7afa030648
Sorin <zece.delfini@outlook.com>
2024-02-06 01:47:15 +0200

    fixed login and signup urls

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -10,11 +10,14 @@ export const useSignup = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("http://localhost/api/user/signup", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
+    const response = await fetch(
+      "https://node-box.onrender.com/api/user/signup",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      }
+    );
     const json = await response.json();
 
     if (!response.ok) {
